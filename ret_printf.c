2ba1e82b4daa94c8edf6f994e2c7925c789410b1
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-11-30 05:23:20 +0900

    save for now

--- ret_printf.c
+++ ret_printf.c
@@ -6,7 +6,16 @@ int main()
 	int num = 999;
 	int count = 0;
 
+	printf("---------------normal ret----------------\n");
 	printf("123456789012345678901234567890\n");
 	count = printf("%s", str);
+	printf("\nret:%d\n\n", count);
+	printf("123456789012345678901234567890\n");
+	count = printf("%d", num);
 	printf("\nret:%d\n", count);
+
+	printf("\n---------------false ret---------------\n");
+	printf("123456789012345678901234567890\n");
+	count = printf("%s", str);
+	printf("\nret:%d\n\n", count);
 }
