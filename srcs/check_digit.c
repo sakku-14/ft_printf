7eb9fd7ebe8baadd7f2152a30ee046e6b261025a
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-12-03 13:16:33 +0900

    [Improve] about d flag but still need improve

--- srcs/check_digit.c
+++ srcs/check_digit.c
@@ -6,7 +6,7 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 16:42:27 by ysakuma           #+#    #+#             */
-/*   Updated: 2020/12/02 16:42:56 by ysakuma          ###   ########.fr       */
+/*   Updated: 2020/12/03 13:14:38 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ int pf_check_digit(int num)
 	int dret;
 
 	dret = 0;
-	if (num > 0)
+	if (num == 0)
+		return (1);
+	else if (num > 0)
 	{
-		while (num > 1)
+		while (num >= 1)
 		{
 			num /= 10;
 			dret++;
@@ -27,7 +29,7 @@ int pf_check_digit(int num)
 	}
 	else
 	{
-		while (num < -1)
+		while (num <= -1)
 		{
 			num /= 10;
 			dret ++;
@@ -41,6 +43,8 @@ int pf_check_usdigit(unsigned int num)
 	int dret;
 
 	dret = 0;
+	if (num == 0)
+		return (0);
 	while (num >= 1)
 	{
 		num /= 10;
@@ -54,6 +58,8 @@ int pf_check_xdigit(unsigned int num)
 	int count;
 
 	count = 0;
+	if (num == 0)
+		return (0);
 	while (num >= 1)
 	{
 		num /= 16;
--- srcs/main2.c
+++
@@ -1,9 +0,0 @@
-#include "../includes/ft_printf.h"
-
-int main()
-{
-	int count = 0;
-	char *str = NULL;
-	count = ft_printf("%10s", str);
-	ft_printf("\n%d\n", count);
-}
