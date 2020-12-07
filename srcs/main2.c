eb33e93ceb1cb3122568e3d34ccff0550059e1b7
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-12-07 16:44:58 +0900

    [Save] for now

--- srcs/main2.c
+++ srcs/main2.c
@@ -2,6 +2,10 @@
 
 int main()
 {
-	ft_printf("% d<-\n", 100);
-	printf("% d<-\n", 100);
+	ft_printf("%.0d\n", 0);
+	printf("%.0d\n", 0);
+	ft_printf("%.0d\n", 100);
+	printf("%.0d\n", 100);
+	ft_printf("%5.0d\n", 100);
+	printf("%5.0d\n", 100);
 }
